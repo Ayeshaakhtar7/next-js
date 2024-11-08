ecd53c91df6049ea7c0f86d480df7a0c22495048
[Ayesha Akhtar] <163725982+Ayeshaakhtar7@users.noreply.github.com>
2024-11-08 21:57:34 +0500

    first commit

---
+++ src/app/grid/grid.tsx
@@ -0,0 +1,10 @@
+export default function Home(){
+    return(
+        <main className="grid grid-cols-3"
+        bg-slate-400>
+            <div>hello grid</div>
+            <div>hello grid</div>
+            <div>hello grid</div>
+        </main>
+    )
+}
---
+++ src/app/page.tsx
@@ -0,0 +1,36 @@
+
+
+
+
+
+import Image from "next/image"
+export default function Home() {
+  return (
+    <main className="mt-10 mx-10">
+
+      {/* Hero Section */}
+
+      <section className="flex gap-6 items-center">
+
+        {/* Left side */}
+
+        <div className="flex-1">
+          <h1 className="font-bold text-3xl ">Hi, I'm Ayesha Rana</h1>
+          <p className="text-lg text-gray-600 mt-2">
+            I'm a full-stack developer with a passion for building beautiful
+            websites and applications.
+            I'm a full-stack developer with a passion for building beautiful
+            websites and applications.
+          </p>
+          <button className="bg-gray-900 text-white py-2 mt-4 px-4" >View My Work</button>
+        </div>
+
+        {/* Right Side */}
+
+        <div className="flex-1">
+          <Image width={500} height={500} src="/pic.png" alt="pic" />
+        </div>
+      </section>
+    </main>
+  );
+}
